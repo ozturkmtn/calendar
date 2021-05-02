33e870e2e71fef1069f75120dbecde323309ce9e
ozturkmtn <Hb%R753jM>
2021-05-02 22:09:03 +0300

    4

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-echo '323';die();
+//echo '323';die();
 
 /**
  * Laravel - A PHP Framework For Web Artisans
@@ -36,7 +36,7 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
